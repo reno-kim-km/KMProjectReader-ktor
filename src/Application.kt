9f6e02cc633f6c9edddcf836553d95b9555a9263
Reno <reno.kim@kinemaster.com>
2021-08-25 21:22:51 +0900

    testing false

--- src/Application.kt
+++ src/Application.kt
@@ -15,9 +15,8 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = true) {
+fun Application.module(testing: Boolean = false) {
     install(AutoHeadResponse)
-    //for test
 
     routing {
         get("/") {
