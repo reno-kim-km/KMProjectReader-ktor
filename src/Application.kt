bb34498c0c5ff9294d9a17afe30e4c78d7e77fe2
Reno <reno.kim@kinemaster.com>
2021-08-30 17:50:56 +0900

    for push

--- src/Application.kt
+++ src/Application.kt
@@ -11,7 +11,7 @@ import io.ktor.routing.*
 import kotlinx.html.*
 import java.io.File
 
-
+//for push
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
